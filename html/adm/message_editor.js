4409a20754658f742588747eb6004d71d8aaae9e
Ilya.Drobenya <Ilya.Drobenya@8f611146-1eef-11df-8469-e5e4969c22d8>
2010-03-09 18:49:14 +0000

    Formatting in message editor was developed.

--- html/adm/message_editor.js
+++ html/adm/message_editor.js
@@ -17,7 +17,16 @@ MessageEditor.prototype.addSmileToMessage = function(smileText) {
         return;
 	}
 
-    this.textArea.value += ' ' + smileText + ' ';
+    this.insertText('', ' ' + smileText + ' ');
+}
+
+
+MessageEditor.prototype.addTagsToMessage = function(startTag, endTag) {
+    if (this.textArea == null) {
+        return;
+	}
+
+    this.insertText(startTag, endTag);
 }
 
 
@@ -30,3 +39,32 @@ MessageEditor.prototype.setTextArea = function(textAreaId) {
 	this.textArea = textArea;
 }
 
+
+// function for store caret position
+MessageEditor.prototype.storeCaret = function() {
+    if (document.selection && document.selection.createRange 
+            && this.textArea) {
+		var textArea = this.textArea;
+        textArea.caretPosition = document.selection.createRange().duplicate();	
+    }
+}
+
+
+// function for insert text into text area around selected text or at end
+MessageEditor.prototype.insertText = function(startText, endText) { 
+    if (this.textArea != null && this.textArea.caretPosition) {
+        this.textArea.caretPosition.text = startText 
+            + this.textArea.caretPosition.text + endText;
+	} 
+    else if (this.textArea != null && this.textArea.selectionStart + 1 
+            && this.textArea.selectionEnd + 1) {
+        var textArea = this.textArea;
+		var selectedText = textArea.value.substring(textArea.selectionStart, textArea.selectionEnd);
+        textArea.value = textArea.value.substring(0, textArea.selectionStart) 
+            + startText + selectedText + endText 
+            + textArea.value.substring(textArea.selectionEnd, textArea.value.length);
+	}
+    else {
+        this.textArea.value += startText + endText;
+    }
+}
