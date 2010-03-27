30dea10889cb7c2d3b58f5bdba71ff48a3c024f8
Ilya.Drobenya <Ilya.Drobenya@8f611146-1eef-11df-8469-e5e4969c22d8>
2010-03-27 21:44:48 +0000

    Rename directory include to includes

--- trunk/config.php
+++ trunk/config.php
@@ -1,14 +1,15 @@
 <?php
 
 
-/** Database configuration */
-define('IS_MYSQL_USED', TRUE);
-
+/** Database configuration */  
 define('DB_USER', 'root');
 
 define('DB_PASSWORD', '');
 
 define('DB_PATH', "localhost:3306");
+
+/** Application configuration */
+define('DEBUG', TRUE);
 
 
 ?>
