2a4e37930635cd0c177a6eae6167c330cf0dee41
Ilya.Drobenya <Ilya.Drobenya@8f611146-1eef-11df-8469-e5e4969c22d8>
2010-03-29 06:51:48 +0000

    Add some comments

--- db/create_smilies_table.sql
+++ db/create_smilies_table.sql
@@ -1,4 +1,9 @@
-﻿SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+﻿-- File contain SQL-code for create table `Smilies` in
+-- database `bit_forum`. This table contain smile alias
+-- and path to smile image.
+-- author: Ilya Drobenya
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL';
 
