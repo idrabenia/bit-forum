8ee57e416327f9fe3c39ce32dabfc19754e012e8
Ilya.Drobenya <Ilya.Drobenya@8f611146-1eef-11df-8469-e5e4969c22d8>
2010-04-13 19:35:29 +0000

    Develop authentication subsystem

--- trunk/config.php
+++ trunk/config.php
@@ -1,17 +1,30 @@
 <?php
 
+/**
+ * File contain application options and
+ * settings.
+ */
 
 /** Database configuration */  
 define('DB_USER', 'root');
 
 define('DB_PASSWORD', '');
 
-define('DB_PATH', "localhost:3306");
+define('DB_PATH', 'localhost:3306');
 
 define('DB_NAME', 'bit_forum');
 
 /** Application configuration */
 define('DEBUG', TRUE);
 
+/* Show errors if necessary */
+if (DEBUG === TRUE) { 
+    ini_set('display_errors', 'On');
+    error_reporting(E_ALL);
+} 
+else {
+	ini_set('display_errors', 'Off');
+	error_reporting(0);
+} // if
 
 ?>
