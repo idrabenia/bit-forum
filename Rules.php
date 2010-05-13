a1fb6ebb483498feb52d09799aca926da899d858
alexei.zhvirblis <alexei.zhvirblis@8f611146-1eef-11df-8469-e5e4969c22d8>
2010-05-13 06:30:48 +0000

    Integrate Rules.php, User_page.php, User_reg.php

---
+++ Rules.php
@@ -0,0 +1,18 @@
+﻿<?php
+	
+	header('content-type: text/html; charset=utf8');
+
+	require_once ("common.php");
+	
+	//----------------------------------GET TEMPLATES-----------------------------------//
+	$main_tpl = file_get_contents('./templates/main.tpl');//Common template for all pages
+	$rules_tpl = file_get_contents('./templates/User/rules.tpl');//List of rules with agreement
+	
+	
+	$main_tpl = str_replace('{TITLE}',"БИТ-форум - Правила форума",$main_tpl);
+	$main_tpl = str_replace('{ROOT_PATH}', ROOT_PATH, $main_tpl);
+	$main_tpl = str_replace('{BODY}', $rules_tpl, $main_tpl);
+		
+	echo $main_tpl;
+
+?> 
--- templates/User/Rules.php
+++
@@ -1,6 +0,0 @@
-<?php
-		
-	$rules_page = file_get_contents('rules.tpl');
-	echo $rules_page;
-
-?> 
